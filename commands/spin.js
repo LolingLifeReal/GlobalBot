75afa956b8ed22c260e8ef5c100d014126006084
LolingLifeReal <userlessgg@gmail.com>
2021-06-17 09:13:10 -0400

    Perms fix

--- commands/spin.js
+++ commands/spin.js
@@ -3,12 +3,15 @@ module.exports = {
     description: "Spins booster prize",
     execute(Discord, client, message, args) {
 
-
-        let i = ["**GFX**", "**Role**", "**Command**", "**Emoji**", "**DJ**"]
-
-        let y = i[Math.floor(i.length * Math.random())]
+        if (message.member.hasPermission("ADMINISTRATOR")) {
 
 
-        message.channel.send(`Your prize is... ${y}`)
+            let i = ["**GFX**", "**Role**", "**Command**", "**Emoji**", "**DJ**"]
+
+            let y = i[Math.floor(i.length * Math.random())]
+
+
+            message.channel.send(`Your prize is... ${y}`)
+        }
     }
 }
