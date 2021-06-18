5e9c606b0b2f0b05af8fcd88502c96cd8de422cf
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-06-17 22:36:01 -0400

    Update hawaii.js

--- commands/hawaii.js
+++ commands/hawaii.js
@@ -3,6 +3,6 @@ moudle.exports = {
 execute(message, args, Discord, client) {
     const RobloxEmbed = new Discord.MessageEmbed()
       .setColor("#30D5C8")
-      .addField("Global Survivor: Classic", "https://www.roblox.com/games/6454408917/Global-Survivor-Hawaii)
+      .addField("Global Survivor: Classic", "https://www.roblox.com/games/6454408917/Global-Survivor-Hawaii")
   }
 }
