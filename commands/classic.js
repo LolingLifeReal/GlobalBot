d7679158b3998c094818d851b727d9c54595796c
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-06-17 22:33:58 -0400

    classic
    e

---
+++ commands/classic.js
@@ -0,0 +1,8 @@
+moudle.exports = {
+  name: "classic"
+execute(message, args, Discord, client) {
+    const RobloxEmbed = new Discord.MessageEmbed()
+      .setColor("#30D5C8")
+      .addField("Global Survivor: Classic", "https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
+  }
+}
