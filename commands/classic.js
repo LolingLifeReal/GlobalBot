ea614daf03af701e80879ac919ea609a64f37a41
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-06-17 22:40:56 -0400

    Update classic.js

--- commands/classic.js
+++ commands/classic.js
@@ -4,5 +4,6 @@ execute(message, args, Discord, client) {
     const RobloxEmbed = new Discord.MessageEmbed()
       .setColor("#30D5C8")
       .addField("Global Survivor: Classic", "https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
+    message.channel.send(RobloxEmbed)
   }
 }
