4096eb84dec0b90f9585c2b3088d071165c3b515
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-06-17 22:45:39 -0400

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -11,7 +11,7 @@ module.exports = {
             .addField("Global Survivor: Murder Island", "\n\n\n https://www.roblox.com/games/6439842558/Global-Survivor-Murder-Island \n\n\n")
             .addField("Global Survivor: South Pole", "\n\n\n https://www.roblox.com/games/6439774225/Global-Survivor-South-Pole \n\n\n")
             .addField("Global Survivor: Wild West", "\n\n\n https://www.roblox.com/games/6736859835/Global-Survivor-Wild-West \n\n\n")
-            .addField("Global Survivor: Homestore", "\n\n\n https://www.roblox.com/games/6736859835/Global-Survivor-Wild-West \n\n\n")
+            //.addField("Global Survivor: Homestore", "\n\n\n https://www.roblox.com/games/6736859835/Global-Survivor-Wild-West \n\n\n")
 
 
         message.channel.send(RobloxEmbed)
