d1ee4343875bc3f8c976aa980f7197c0affd9db1
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-08-01 03:35:53 -0400

    3

--- commands/hawaii.js
+++ commands/hawaii.js
@@ -1,9 +1,6 @@
 module.exports = {
   name: "hawaii",
 execute(message, args, Discord, client) {
-    const RobloxEmbed = new Discord.MessageEmbed()
-      .setColor("#30D5C8")
-      .addField("Global Survivor: Hawaii", "https://www.roblox.com/games/6454408917/Global-Survivor-Hawaii")
-    message.channel.send(RobloxEmbed)
+    message.channel.send("Global Survivor: Hawaii, https://www.roblox.com/games/6454408917/Global-Survivor-Hawaii")
   }
 }
