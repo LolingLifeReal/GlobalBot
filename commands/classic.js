a7bd2bcfcb327d1d128896f15918db534eff7293
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-08-01 20:48:44 -0400

    Ass

--- commands/classic.js
+++ commands/classic.js
@@ -1,9 +1,6 @@
 module.exports = {
   name: "classic",
 execute(message, args, Discord, client) {
-    const RobloxEmbed = new Discord.MessageEmbed()
-      .setColor("#30D5C8")
-      .addField("Global Survivor: Classic", "https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
-    message.channel.send(RobloxEmbed)
+    message.channel.send("Global Survivor: Classic", "https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
   }
 }
