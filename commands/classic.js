7f8bed6f9660dcc29dc9038845aa9aafa6498a30
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-08-01 20:54:40 -0400

    Update classic.js
    D

--- commands/classic.js
+++ commands/classic.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "classic",
 execute(message, args, Discord, client) {
-    message.channel.send("Global Survivor: Classic", "https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
+    message.channel.send("Global Survivor: Classic, https://www.roblox.com/games/6439796398/Global-Survivor-Classic?")
   }
 }
