96c8df5cf0300f13db3388b3c242220729b958bc
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-08-08 23:41:04 -0400

    Update gracie.js

--- commands/gracie.js
+++ commands/gracie.js
@@ -1,8 +1,6 @@
 module.exports = {
     name: 'gracie',
-    execute(message, args, Discord, client) {
-
-        
-            message.react('<a:Tick:865695136616022046>');
+    execute(message, args, Discord, client) 
+        message.channel.send("🥜🍒");
     }
 }
