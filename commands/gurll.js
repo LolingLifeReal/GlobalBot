e34267ad0c04be7414bad1466a48cc2fa68e07e3
LolingLifeReal <83317321+LolingLifeReal@users.noreply.github.com>
2021-08-11 01:13:56 -0400

    Create gurll.js

---
+++ commands/gurll.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "gurll",
+execute(message, args, Discord, client) {
+    message.channel.send("Gurl, didn't know about mobile I'm not a phone user. You were giving a job as a developer and you choose to act like a child? In a semi-professional group chat. My thought process was thinking it would be unnecessary, however because you have the shortest temper in the world you can't just calmly reply back with a normal human response its quite hard to work with you and its probably why nope is ignoring you. I didn't want people to do unnecessary things if u just told me that mobile cant without the block part maybe people would respect u more and listen to ur ideas. Im done with this rant I just want you to know how you are acting because you arent the only developer doing things and everyone is just as important.")
+  }
+}
